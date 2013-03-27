bb289033a37012d09756ead51654a98f1bdd8b77
Samuel Cole <sam@samuelcole.name>
2013-03-27 10:58:47 -0400

    Check in pollster.js

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-pollster
+Pollster
 ========
+
+Pollster is a small library for doing repeated polling events:
+
+    var pollster = new Pollster('/url/to/ping', function (data) {
+      if (data.stop_polling === true) {
+        return true;
+      } else {
+        return false;
+      }
+    });
+
+Dependencies
+-----------
+
+jQuery. I only use it for the XHR. I would love to port this to just
+a vanilla XMLHttpRequest.
+
+Contributers
+------------
+
+* Samuel Cole <sam@samuelcole.name>
+* Lance Ivy <lance@cainlevy.net>
---
+++ src/jquery.pollster.js
@@ -0,0 +1,80 @@
+(function () {
+
+  //The timeout has to live in this scope
+  var timeout;
+
+  function Pollster(url, callback, options) {
+    this.url = url;
+    this.callback = callback || function () { return; };
+    this.options = $.extend({
+      delay: 30 * 1000,
+      on_error: function () { return; },
+      on_complete: function () { return; },
+      continue_on_error: false
+    }, options);
+    this.xhr = false;
+    this.running = false;
+    this.go();
+  }
+
+  if (typeof jQuery !== 'undefined') {
+    jQuery.pollster = function (url, callback, options) {
+      return new Pollster(url, callback, options);
+    };
+  }
+
+  window.Pollster = Pollster;
+
+  Pollster.prototype.start = function () {
+    var _this = this;
+    this.running = true;
+    timeout = setTimeout(function () {
+      _this.go();
+    }, this.options.delay);
+  };
+
+  Pollster.prototype.stop = function () {
+    this.running = false;
+    clearTimeout(timeout);
+  };
+
+  Pollster.prototype.go = function () {
+    var stop = false,
+      _this = this;
+
+    // wait for existing request to complete
+    if (this.xhr) { return; }
+
+    function cont() {
+      if (stop) {
+        _this.stop();
+      } else {
+        _this.start();
+      }
+    }
+
+    //TODO: get rid of this jQuery dependecy
+    this.xhr = jQuery.ajax({
+      url: this.url,
+      dataType: 'json',
+      type: 'get',
+      global: false,
+      success: function (data) {
+        stop = _this.callback(data);
+        cont();
+      },
+      error: function () {
+        if (_this.options.continue_on_error) {
+          cont();
+        }
+        _this.options.on_error();
+      },
+      complete: function () {
+        _this.xhr = false;
+        _this.options.on_complete();
+      }
+    });
+
+  };
+
+}());
