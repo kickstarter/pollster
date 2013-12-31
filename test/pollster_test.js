e686b7003bdf4c1d8045232a08ed171abef895e3
Samuel Cole <sam@samuelcole.name>
2013-12-31 16:30:47 -0500

    Tests!

--- test/pollster_test.js
+++ test/pollster_test.js
@@ -1,4 +1,4 @@
-(function($) {
+(function() {
   /*
     ======== A Handy Little QUnit Reference ========
     http://api.qunitjs.com/
@@ -20,16 +20,47 @@
       throws(block, [expected], [message])
   */
 
-  module('jQuery#awesome', {
-    // This will run before each test in this module.
-    setup: function() {
-      this.elems = $('#qunit-fixture').children();
+  module('pollster', {
+    setup: function () {
+      this.makeServer = function () {
+        this.server = this.sandbox.useFakeServer();
+        this.server.respondWith([200, { "Content-Type": "application/json" },
+                           '["item1", "item2"]']);
+      };
+      this.waitAndRespond = function () {
+        this.clock.tick(30 * 1000);
+        this.server.respond();
+      };
     }
   });
 
   test('is a function', function() {
     expect(1);
     ok(typeof Pollster === 'function');
+  });
+
+  test('polls at least once', function() {
+    this.makeServer();
+    var callback = this.spy();
+    new Pollster('/ping', callback);
+    this.waitAndRespond();
+    ok(callback.calledOnce);
+  });
+
+  test('stops polling when the callback returns true', function() {
+    this.makeServer();
+    var response = false;
+    var callback = function () {
+      return response;
+    };
+    callback = this.spy(callback);
+    new Pollster('/ping', callback);
+    this.waitAndRespond();
+    response = true;
+    this.waitAndRespond();
+    this.waitAndRespond();
+
+    ok(callback.calledTwice);
   });
 
 }(jQuery));
