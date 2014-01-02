090ff15a0858135249e24e2918b9afcd8c031e38
Samuel Cole <sam@samuelcole.name>
2014-01-02 11:41:35 -0500

    more tests

--- test/pollster_test.js
+++ test/pollster_test.js
@@ -47,6 +47,14 @@
     ok(callback.calledOnce);
   });
 
+  test('callback recieves data from server', function() {
+    this.makeServer();
+    var callback = this.spy();
+    new Pollster('/ping', callback);
+    this.waitAndRespond();
+    strictEqual(callback.args[0][0].length, 2);
+  });
+
   test('stops polling when the callback returns true', function() {
     this.makeServer();
     var response = false;
@@ -60,6 +68,53 @@
     this.waitAndRespond();
     this.waitAndRespond();
 
+    ok(callback.calledTwice);
+  });
+
+  test('stops polling on server error', function() {
+    this.makeServer();
+    var callback = this.spy();
+    new Pollster('/ping', callback);
+    this.waitAndRespond();
+    this.server.respondWith([500, {}, ""]);
+    this.waitAndRespond();
+    ok(callback.calledOnce);
+  });
+
+  test('does not stop polling on error if continue_on_error is set', function() {
+    this.makeServer();
+    var callback = this.spy();
+    new Pollster('/ping', callback, {
+      continue_on_error: true
+    });
+    this.waitAndRespond();
+    this.server.respondWith([500, {}, ""]);
+    this.waitAndRespond();
+    this.server.respondWith([200, {}, "[]"]);
+    this.waitAndRespond();
+    ok(callback.calledTwice);
+  });
+
+  test('calls on_error on error', function() {
+    this.makeServer();
+    var callback = this.spy();
+    new Pollster('/ping', function () { }, {
+      on_error: callback
+    });
+    this.server.respondWith([500, {}, ""]);
+    this.waitAndRespond();
+    ok(callback.calledOnce);
+  });
+
+  test('calls on_complete on success or error', function() {
+    this.makeServer();
+    var callback = this.spy();
+    new Pollster('/ping', function () { }, {
+      on_complete: callback
+    });
+    this.waitAndRespond();
+    this.server.respondWith([500, {}, ""]);
+    this.waitAndRespond();
     ok(callback.calledTwice);
   });
 
